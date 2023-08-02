3285c928de6d9634b8462c50150e17206bf67c76
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-08-02 22:38:32 +0300

    feat: Writing a basic server; Hello World

---
+++ web_examples/hello_world/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+/*
+	a handler receives two parameters, the ResponseWRiter to write text / html responses
+	a Request which contains all information about this HTTP request (URL ? header fields)
+*/
+
+func main() {
+	// Reggister a request handler that receives all incoming HTTP Connections
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	})
+
+	// Listen to connections on the port
+	http.ListenAndServe(":80", nil)
+}
