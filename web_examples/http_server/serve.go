5a55706216151236ce53ceaa0707c0c701a25016
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-08-03 00:12:34 +0300

    feat: Dyanamic & Static response handling

---
+++ web_examples/http_server/serve.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+/*
+	basics of creating a server in Go, should :
+	1. Process Dynamic Requests
+	2. Serve static assets
+	3. Accept connections
+*/
+
+
+func main() {
+	// register a new handler : takes the path to match & function ot execute (Dynamic Request)
+	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to my website!")
+	})
+
+	// Serving static assets
+	fs := http.FileServer(http.Dir("static/")) // use inbuilt FileServer & point to the url path(to serve files from)
+	http.Handle("/static/", http.StripPrefix("/static/", fs)) // to serve file, strip url prefix(the name of the dir where tehe files are)
+
+	// Accept connections
+	http.ListenAndServe(":80", nil)
+}
