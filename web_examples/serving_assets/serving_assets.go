fbaa1e8c09962e97406f5a0ad362472d26b2a5e2
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-08-04 20:24:19 +0300

    chore: Comments on running file

--- web_examples/serving_assets/serving_assets.go
+++ web_examples/serving_assets/serving_assets.go
@@ -4,6 +4,13 @@ import (
 	"net/http"
 )
 
+/*
+	to run :
+		Change to main package
+		go run serving_assets.go
+		curl -s http://localhost:8080/static/css/styles.css
+*/
+
 func ServingAssets() {
 	fs := http.FileServer(http.Dir("assets/"))
 	http.Handle("/static", http.StripPrefix("/static/", fs))
