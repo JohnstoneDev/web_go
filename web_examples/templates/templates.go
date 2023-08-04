97ed58ea864e014b81c9834297cae1dee46f03ac
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-08-04 19:18:41 +0300

    feat: Templates

---
+++ web_examples/templates/templates.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"net/http"
+	"html/template" // package provides rich templating language for html templates
+)
+
+type Todo struct {
+	Title string
+	Done bool
+}
+
+type TodoPageData struct {
+	PageTitle string
+	Todos []Todo
+}
+
+func Templates() {
+	// Parse a template
+	tmpl := template.Must(template.ParseFiles("layout.html"))
+
+	// execute a template in a request handle
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// define data (A todo list & page title to be passed into a template)
+		data := TodoPageData{
+			PageTitle: "My TODO list",
+			Todos: []Todo{
+					{Title: "Task 1", Done: false},
+					{Title: "Task 2", Done: true},
+					{Title: "Task 3", Done: true},
+			},
+	}
+		tmpl.Execute(w, data)
+	})
+
+	http.ListenAndServe(":80", nil)
+}
+
+func main() {
+	Templates()
+}
